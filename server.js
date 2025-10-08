eff2b3d94a2c00dfe538eb8564dd7d74762e9ff9
GiangNguyen111 <minhsonn23@gmail.com>
2025-10-09 05:15:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("./"));
 app.use((req, res, next) => {
-  res.setHeader("Cache-Control", "no-store");
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
   next();
 });
 
